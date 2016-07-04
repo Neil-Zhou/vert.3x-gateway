caa294687b29c19429c68d48087c71152702c056
chenyang <cy@stlc.cn>
2016-07-04 17:49:38 +0800

    support zk for register server

---
+++ src/main/java/com/stone/registration/ServiceRegistry.java
@@ -0,0 +1,13 @@
+package com.stone.registration;
+/**
+ * The Service registry knows all service verticles, a verticle registers here and will be traced. The registry also notify the router to add/remove routes to the services.
+ * Created by young on 16/7/4.
+ */
+public interface ServiceRegistry {
+
+    public void registerService(String name, String uri);
+
+    public void unregisterService(String name, String uri);
+
+    public String discoverServiceURI(String name);
+}
---
+++ src/main/java/com/stone/registration/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by young on 16/7/4.
+ */
+package com.stone.registration;
