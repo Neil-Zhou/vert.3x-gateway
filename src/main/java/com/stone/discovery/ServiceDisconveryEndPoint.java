71ff762e98442f1f900a03dc0601a02a61afb233
chenyang <cy@stlc.cn>
2016-07-05 08:14:15 +0800

    support zk for discovery service

---
+++ src/main/java/com/stone/discovery/ServiceDisconveryEndPoint.java
@@ -0,0 +1,10 @@
+package com.stone.discovery;
+
+/**
+ * Created by young on 16/7/5.
+ */
+public abstract  class ServiceDisconveryEndPoint {
+
+
+    public abstract void discovery(String name);
+}
